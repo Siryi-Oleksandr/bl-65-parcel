52c253a511227b7a8e387b23db015a201ea5d4f6
Siryi-Oleksandr <108647823+Siryi-Oleksandr@users.noreply.github.com>
2022-10-11 08:32:24 +0000

    Deploying to gh-pages from @ Siryi-Oleksandr/bl-65-parcel@088bd6363cea9b858bee8327d283ace37e3d4238 🚀

---
+++ index.28efb76c.js
@@ -0,0 +1,2 @@
+$(".kickstart__list").slick({infinite:!0,slidesToShow:5,slidesToScroll:3,nextArrow:".slider__next",prevArrow:".slider__previous"});
+//# sourceMappingURL=index.28efb76c.js.map
---
+++ index.62c05caf.js
@@ -0,0 +1,2 @@
+$(".kickstart__list").slick({infinite:!0,slidesToShow:5,slidesToScroll:3,nextArrow:".slider__next",prevArrow:".slider__previous"});
+//# sourceMappingURL=index.62c05caf.js.map
